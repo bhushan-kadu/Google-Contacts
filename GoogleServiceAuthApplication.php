4ffd152b7b854f0a7ca99f61f2136b6a819c0701
Bhushan D. Kadu <bkadu3@gmail.com>
2020-03-28 21:03:27 +0530

    Add files via upload

---
+++ GoogleServiceAuthApplication.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace GoogleCustom;
+
+use Google_Client;
+
+class GoogleServiceAuthApplication {
+  /**
+   * Google client.
+   *
+   * @var array
+   */
+  protected $client;
+
+  public function __construct($conf) {
+    if (isset($conf['google_auth_file'])) {
+      putenv('GOOGLE_APPLICATION_CREDENTIALS=' . $conf['google_auth_file']);
+    }
+    $this->client = new Google_Client();
+    $this->client->useApplicationDefaultCredentials();
+    $this->client->setScopes($conf['gcontacts_scopes']);
+    $this->client->setSubject($conf['gcontacts_account']);
+  }
+
+  /**
+   * @return array
+   */
+  public function getClient() {
+    return $this->client;
+  }
+}
+      
